eb5bd57c13efd6e3971a51f862096a7928343857
DanielaG0627 <danielagb1790@gmail.com>
2021-05-11 21:34:40 -0400

    Adding Python_Practice and Resources foldder

---
+++ Resources/PyPoll.py
@@ -0,0 +1,25 @@
+#The data we need to retreive:
+# 1. Total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. Total number of votes each candidate received
+# 4. Percentage of votes each candidate won
+# 5. The winner of the election based on popular vote
+
+
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = 'Documents/BootCamp/git/Module3/election-analysis/Resources/election_results.csv'
+# Assign a variable to save the file to a path.
+file_to_save = 'Documents/BootCamp/git/Module3/election-analysis/Resources/analysis/election_analysis.txt'
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+     # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+
+    # Print the header row.
+    headers = next(file_reader)
+    print(headers)
